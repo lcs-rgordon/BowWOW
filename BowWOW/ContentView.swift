e8b2f02b17d451fb5d5b94b00ae571cc6fa2e6f2
Russell Gordon <rgordon@lcs.on.ca>
2020-11-24 20:00:24 -0500

    Basic version – shows layout using an image.
    Images from https://dog.ceo

--- BowWOW/ContentView.swift
+++ BowWOW/ContentView.swift
@@ -5,13 +5,26 @@
 //  Created by Russell Gordon on 2020-11-24.
 //
 
+// Dog CEO API
+// https://dog.ceo/dog-api/
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Image("example")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                Spacer()
+            }
+            .navigationTitle("Bow WOW")
+        }
+        
     }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
