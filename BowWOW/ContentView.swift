547ab386af3b7e919fc269beb6906fe5343a721c
Russell Gordon <rgordon@lcs.on.ca>
2020-11-24 20:05:52 -0500

    Basic request to get a random dog pic works.
    Next: parse the response and then retrieve the actual image.

--- BowWOW/ContentView.swift
+++ BowWOW/ContentView.swift
@@ -11,6 +11,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -22,6 +23,37 @@ struct ContentView: View {
             }
             .navigationTitle("Bow WOW")
         }
+        .onAppear() {
+            fetchMoreCuteness()
+        }
+    }
+    
+    // Get a random pooch pic!
+    func fetchMoreCuteness() {
+        
+        // 1. Prepare a URLRequest to send our encoded data as JSON
+        let url = URL(string: "https://dog.ceo/api/breeds/image/random")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "GET"
+        
+        // 2. Run the request and process the response
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            // handle the result here – attempt to unwrap optional data provided by task
+            guard let doggieData = data else {
+                
+                // Show the error message
+                print("No data in response: \(error?.localizedDescription ?? "Unknown error")")
+                
+                return
+            }
+            
+            // It seems to have worked? Let's see what we have
+            print(String(data: doggieData, encoding: .utf8)!)
+                        
+        }.resume()
+
         
     }
         
